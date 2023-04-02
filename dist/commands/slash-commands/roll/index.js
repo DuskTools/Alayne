1de6e9091cf91a11c21e7f4e606a4774307fb09e
alxjrvs@gmail.com <alxjrvs@gmail.com>
2023-04-02 12:30:58 -0400

    Build

--- dist/commands/slash-commands/roll/index.js
+++ dist/commands/slash-commands/roll/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.roll = void 0;
+const builders_1 = require("@discordjs/builders");
 const randsum_1 = require("randsum");
 const generateRollResult = (quantity) => {
     const sides = 6;
@@ -22,27 +23,26 @@ const generateRollResult = (quantity) => {
     });
 };
 const getSuccessString = (type) => {
-    const responseArray = ['====================================='];
+    const responseArray = [];
     switch (type) {
         case 'critical':
-            responseArray.push('|                        __**Critical Success**__                         |');
-            responseArray.push('|          *Things go better than expected*           |');
+            responseArray.push('__**Critical Success**__');
+            responseArray.push('*Things go better than expected*');
             break;
         case 'success':
-            responseArray.push('|                                 __**Success**__                                |');
-            responseArray.push('|                            *Things go well*                          |');
+            responseArray.push('__**Success**__');
+            responseArray.push('*Things go well*');
             break;
         case 'partial':
-            responseArray.push('|                        __**Partial Success**__                          |');
-            responseArray.push('|        *Things go well, but not perfectly*           |');
+            responseArray.push('__**Partial Success**__');
+            responseArray.push('*Things go well, but not perfectly*');
             break;
         case 'failure':
-            responseArray.push('|                                __**Failure**__                                   |');
-            responseArray.push('|                       *Things go poorly*                          |');
+            responseArray.push('__**Failure**__');
+            responseArray.push('*Things go poorly*');
             break;
     }
-    responseArray.push('=====================================');
-    return responseArray.join('\n');
+    return responseArray;
 };
 const getBladesRollType = (result, quantity) => {
     if (result.total === 6) {
@@ -72,23 +72,66 @@ const parseRolls = (result, bladesSuccess) => {
 const getExplanation = (quantity, user) => {
     const isZero = quantity === 0;
     return [
-        `${user} rolls ${isZero ? 2 : quantity} D6`,
-        `and takes the ${isZero ? 'lowest' : 'highest'} result...`
-    ].join('\n');
+        `${user.username} rolled ${isZero ? 2 : quantity} D6`,
+        `and took the ${isZero ? 'lowest' : 'highest'} result`
+    ];
 };
-async function roll(interaction) {
+const color = (type) => {
+    switch (type) {
+        case 'critical':
+            return 0xffd700;
+        case 'success':
+            return 0x00ff00;
+        case 'partial':
+            return 0xcaffca;
+        case 'failure':
+            return 0xff0000;
+    }
+};
+const thumbnailImage = (total, type) => {
+    const root = 'https://raw.githubusercontent.com/alxjrvs/bladesinthediscord/main/src/assets/d6/';
+    switch (total) {
+        case 1:
+            return `${root}one.png`;
+        case 2:
+            return `${root}two.png`;
+        case 3:
+            return `${root}three.png`;
+        case 4:
+            return `${root}four.png`;
+        case 5:
+            return `${root}five.png`;
+        case 6:
+            if (type === 'critical') {
+                return `${root}double6.png`;
+            }
+            return `${root}six.png`;
+    }
+    throw new Error('Invalid total');
+};
+const buildEmbed = (interaction) => {
     const diceArg = interaction.options.getInteger('dice_pool');
     const quantity = diceArg === 0 ? 0 : diceArg || 1;
     const result = generateRollResult(quantity);
     const bladesSuccess = getBladesRollType(result, quantity);
-    interaction.reply({
-        content: [
-            `*${getExplanation(quantity, interaction.user)}*`,
-            '',
-            `[${parseRolls(result, bladesSuccess)}] => ** ${result.total} ** `,
-            '',
-            getSuccessString(bladesSuccess)
-        ].join('\n')
-    });
+    const [explanationTitle, explanationValue] = getExplanation(quantity, interaction.user);
+    const [successTitle, successValue] = getSuccessString(bladesSuccess);
+    const thumbnail = thumbnailImage(result.total, bladesSuccess);
+    return new builders_1.EmbedBuilder()
+        .setColor(color(bladesSuccess))
+        .setTitle(successTitle)
+        .setDescription(successValue)
+        .setThumbnail(thumbnail)
+        .addFields({ name: '\u200B', value: '\u200B' })
+        .addFields({ name: explanationTitle, value: explanationValue })
+        .addFields({
+        name: 'Rolls',
+        value: `[${parseRolls(result, bladesSuccess)}]`,
+        inline: true
+    }, { name: 'Total', value: `** ${result.total} **`, inline: true });
+};
+async function roll(interaction) {
+    const embed = buildEmbed(interaction);
+    interaction.reply({ embeds: [embed] });
 }
 exports.roll = roll;
