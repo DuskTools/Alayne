3be539036fe56a5da64b65cc1dbfb6434bedf46d
alxjrvs@gmail.com <alxjrvs@gmail.com>
2023-04-15 00:04:15 -0400

    new build

--- dist/commands/slash-commands/utils/findClocks.js
+++ dist/commands/slash-commands/utils/findClocks.js
@@ -1,13 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.findClocks = void 0;
-const discord_js_1 = require("discord.js");
 const constants_1 = require("../clock/constants");
+const messageArr = Array.from({ length: 5 }, () => ({}));
 const findClocks = async (interaction) => {
-    const messageList = await interaction.channel?.messages.fetch({ cache: true, limit: 100 });
-    if (messageList == undefined) {
-        return new discord_js_1.Collection();
-    }
+    const messageList = await messageArr.reduce(async (accumulator, _current, index) => {
+        const resolvedAccumulator = await accumulator;
+        if (index > 0 && resolvedAccumulator.length < 100 * index) {
+            return resolvedAccumulator;
+        }
+        const lastMessage = resolvedAccumulator[resolvedAccumulator.length - 1];
+        const messageList = await interaction.channel?.messages.fetch({
+            cache: true,
+            limit: 100,
+            before: lastMessage?.id
+        });
+        return [...resolvedAccumulator, ...(messageList?.values() || [])];
+    }, Promise.resolve([]));
     return messageList.filter((message) => message.embeds[0] &&
         message.embeds[0].footer?.text === constants_1.RunningClockFooter &&
         message.author.bot &&
