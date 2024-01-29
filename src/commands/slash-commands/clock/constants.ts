9ae744518cf26838a1fc7a0a90a1d05dba3cfd78
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-01-29 00:01:42 -0500

    No More Footertext

--- src/commands/slash-commands/clock/constants.ts
+++
@@ -1,2 +0,0 @@
-export const RunningClockFooter = 'A Blades in the Darkscord Clock'
-export const StoppedClockFooter = 'A Stopped Blades in the Darkcord Clock'
--- src/commands/slash-commands/clock/types.ts
+++ src/commands/slash-commands/clock/types.ts
@@ -2,7 +2,6 @@ export type ClockOptions = {
   name: string
   segments: number
   progress: number
-  footerText?: string
   discordGuildId: string
   link: string
   active: boolean
