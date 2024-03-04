01bc453de70db60f9c186df1ff1b2e772ec4a802
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-03-03 21:17:18 -0500

    WIP - files

--- dist/commands/slash-commands/handleClockButtons/clockNameLink.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clockNameLink = void 0;
-const clockNameLink = (name, link) => `[${name}](${link})`;
-exports.clockNameLink = clockNameLink;
