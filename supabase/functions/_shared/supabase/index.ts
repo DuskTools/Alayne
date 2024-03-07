8768398f4d6039d8dd350b562863fe558b63e332
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-03-07 09:38:29 -0500

    Rename keys

--- supabase/functions/_shared/supabase/index.ts
+++ supabase/functions/_shared/supabase/index.ts
@@ -2,9 +2,9 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 import { Database } from "./database.types.ts"
 
-const URL = Deno.env.get("API_URL")!
-const ANON = Deno.env.get("API_PUBLIC_ANON_KEY")!
-const ADMIN = Deno.env.get("API_PUBLIC_SERVICE_ROLE_KEY")!
+const URL = Deno.env.get("SUPABASE_URL")!
+const ANON = Deno.env.get("SUPABASE_ANON_KEY")!
+const ADMIN = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
 
 export const anonClient = createClient<Database>(
   URL,
