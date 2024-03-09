7d43f0b4b0cbc910c5cad69cd136b689495071bc
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-03-09 17:08:35 -0500

    Boom

---
+++ supabase/functions/send-message-to-notification-channel/index.ts
@@ -0,0 +1,24 @@
+import { json, serve } from "https://deno.land/x/sift@0.6.0/mod.ts"
+
+import { REST } from "npm:@discordjs/rest"
+import { Routes } from "npm:discord-api-types/v10"
+
+serve({
+  "/send-message-to-notification-channel": sendMessageToNotificationChannel,
+})
+
+async function sendMessageToNotificationChannel(request: Request) {
+  const { notification_channel } = await request.json() as {
+    notification_channel: string
+  }
+
+  const discordRest = new REST({ version: "10" }).setToken(
+    Deno.env.get("DISCORD_BOT_TOKEN")!,
+  )
+
+  const response = await discordRest.post(
+    Routes.channelMessages(notification_channel),
+  )
+
+  return json({ response }, { status: 200 })
+}
