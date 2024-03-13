9026d716ed3e3912dad0c8af4cbb27aa69a4d38c
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-03-12 23:26:29 -0400

    Geting closer

--- supabase/functions/dusktools/_shared/deferredResponse.ts
+++ supabase/functions/dusktools/_shared/deferredResponse.ts
@@ -1,13 +1,12 @@
 import { InteractionResponseType } from "https://deno.land/x/discord_api_types@0.37.71/v10.ts"
 import { json } from "https://deno.land/x/sift@0.6.0/mod.ts"
 
-export default (callback: () => unknown) => {
-  Promise.resolve(callback).then((response) => {
-    console.log("Deferred response sent")
+export default (callback: () => Record<string, unknown>) => {
+  Promise.resolve(callback()).then((response) => {
+    console.log("Inside Deferred response")
     console.log(response)
   })
-
-  console.log("Outside response")
+  console.log("Initial response")
 
   return json({
     type: InteractionResponseType.DeferredChannelMessageWithSource,
