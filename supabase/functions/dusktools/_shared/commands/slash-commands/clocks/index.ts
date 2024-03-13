c7941c1cdcd67ca0ac676373ff35f8d366199374
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-03-13 00:59:52 -0400

    use deferred

--- supabase/functions/dusktools/_shared/commands/slash-commands/clocks/index.ts
+++ supabase/functions/dusktools/_shared/commands/slash-commands/clocks/index.ts
@@ -1,44 +1,49 @@
-import { json } from "https://deno.land/x/sift@0.6.0/mod.ts"
 import {
   APIApplicationCommandInteraction,
-  InteractionResponseType,
 } from "https://deno.land/x/discord_api_types@0.37.71/v10.ts"
 
 import ClockService from "../../../../../_shared/services/ClockService.ts"
 import CampaignService from "../../../../../_shared/services/CampaignService.ts"
 import { buildClockMessageOptions } from "../utils/buildClockMessageOptions.ts"
+import deferredResponse from "../../../deferredResponse.ts"
 
 const handleClocks = async (interaction: APIApplicationCommandInteraction) => {
-  const discord_guild_id = interaction.guild_id!
-  const campaign = await CampaignService.findByDiscordGuildId(
-    { discord_guild_id },
-  )
-  if (!campaign) {
-    return json({
-      type: InteractionResponseType.ChannelMessageWithSource,
-      data: {
-        content:
-          "This Campaign has not been initialized! Use /init to initialize it.",
+  return deferredResponse(async () => {
+    const application_id = interaction.application_id
+    const interaction_token = interaction.token
+    const discord_guild_id = interaction.guild_id!
+    const campaign = await CampaignService.findByDiscordGuildId(
+      { discord_guild_id },
+    )
+    if (!campaign) {
+      return {
+        application_id,
+        interaction_token,
+        body: {
+          content:
+            "This Campaign has not been initialized! Use /init to initialize it.",
+          flags: 1 << 6,
+        },
+      }
+    }
+    const clocks = await ClockService.getActiveClocks(campaign.id)
+    const embeds = clocks.map((clockOptions) => {
+      return buildClockMessageOptions(clockOptions).embeds[0]
+    })
+
+    const content = embeds.length > 0
+      ? { embeds }
+      : { content: "No clocks found!" }
+
+    return {
+      application_id,
+      interaction_token,
+      body: {
+        ...content,
         flags: 1 << 6,
       },
-    })
-  }
-  const clocks = await ClockService.getActiveClocks(campaign.id)
-  const embeds = clocks.map((clockOptions) => {
-    return buildClockMessageOptions(clockOptions).embeds[0]
-  })
-
-  const content = embeds.length > 0
-    ? { embeds }
-    : { content: "No clocks found!" }
-
-  return json({
-    type: InteractionResponseType.ChannelMessageWithSource,
-    data: {
-      ...content,
-      flags: 1 << 6,
-    },
-  })
+    }
+  }, true)
 }
 
 export default handleClocks
--- supabase/functions/dusktools/_shared/commands/slash-commands/init/index.ts
+++ supabase/functions/dusktools/_shared/commands/slash-commands/init/index.ts
@@ -1,50 +1,55 @@
-import { json } from "https://deno.land/x/sift@0.6.0/mod.ts"
 import {
   APIApplicationCommandInteraction,
-  InteractionResponseType,
 } from "https://deno.land/x/discord_api_types@0.37.71/v10.ts"
 
 import CampaignService from "../../../../../_shared/services/CampaignService.ts"
 import { adminClient } from "../../../../../_shared/supabase/index.ts"
+import deferredResponse from "../../../deferredResponse.ts"
 
-const handleInit = async (interaction: APIApplicationCommandInteraction) => {
-  const discord_guild_id = interaction.guild_id!
-  const discord_user_id = interaction.member?.user.id!
-  const notification_channel =
-    (interaction.data as unknown as { options: [{ value: string }] }).options
-      ?.[0]?.value
+const handleInit = (interaction: APIApplicationCommandInteraction) => {
+  return deferredResponse(async () => {
+    const application_id = interaction.application_id
+    const interaction_token = interaction.token
+    const discord_guild_id = interaction.guild_id!
+    const discord_user_id = interaction.member?.user.id!
+    const notification_channel =
+      (interaction.data as unknown as { options: [{ value: string }] }).options
+        ?.[0]?.value
 
-  const { campaign: newCampaign, error, joinError } = await CampaignService
-    .create({
-      discord_guild_id,
-      notification_channel,
-      name: "TEMP = " + discord_guild_id,
-    }, { discord_id: discord_user_id! })
+    const { campaign: newCampaign, error, joinError } = await CampaignService
+      .create({
+        discord_guild_id,
+        notification_channel,
+        name: "TEMP = " + discord_guild_id,
+      }, { discord_id: discord_user_id! })
 
-  if (error || joinError) {
-    return json({
-      type: InteractionResponseType.ChannelMessageWithSource,
-      data: {
-        content: error?.code === "23505"
-          ? "This Server has already been registered!"
-          : error || "Error creating campaign",
+    if (error || joinError) {
+      return {
+        application_id,
+        interaction_token,
+        body: {
+          content: error?.code === "23505"
+            ? "This Server has already been registered!"
+            : error || "Error creating campaign",
+          flags: 1 << 6,
+        },
+      }
+    }
+
+    adminClient.functions.invoke("sync-campaign-with-discord", {
+      body: { discord_guild_id },
+    })
+
+    const content = `Campaign Created! ${newCampaign.id}`
+    return {
+      application_id,
+      interaction_token,
+      body: {
+        content,
         flags: 1 << 6,
       },
-    })
-  }
-
-  adminClient.functions.invoke("sync-campaign-with-discord", {
-    body: { discord_guild_id },
-  })
-
-  const content = `Campaign Created! ${newCampaign.id}`
-  return json({
-    type: InteractionResponseType.ChannelMessageWithSource,
-    data: {
-      content,
-      flags: 1 << 6,
-    },
-  })
+    }
+  }, true)
 }
 
 export default handleInit
