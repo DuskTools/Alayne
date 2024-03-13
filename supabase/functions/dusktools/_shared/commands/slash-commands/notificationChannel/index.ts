d8b1821fa4bdb887ad835146c84f48599a38168d
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-03-13 00:43:23 -0400

    WIP

--- supabase/functions/dusktools/_shared/commands/slash-commands/notificationChannel/index.ts
+++ supabase/functions/dusktools/_shared/commands/slash-commands/notificationChannel/index.ts
@@ -1,43 +1,48 @@
-import { json } from "https://deno.land/x/sift@0.6.0/mod.ts"
 import {
   APIApplicationCommandInteraction,
-  InteractionResponseType,
 } from "https://deno.land/x/discord_api_types@0.37.71/v10.ts"
 
 import CampaignService from "../../../../../_shared/services/CampaignService.ts"
+import deferredResponse from "../../../deferredResponse.ts"
 
 const handleNotificationChannel = async (
   interaction: APIApplicationCommandInteraction,
 ) => {
-  const discord_guild_id = interaction.guild_id!
-  const notification_channel =
-    (interaction.data as unknown as { options: [{ value: string }] }).options
-      ?.[0]?.value
+  return deferredResponse(async () => {
+    const application_id = interaction.application_id
+    const interaction_token = interaction.token
 
-  const { error } = await CampaignService
-    .update(
-      discord_guild_id,
-      { notification_channel },
-    )
+    const discord_guild_id = interaction.guild_id!
+    const notification_channel =
+      (interaction.data as unknown as { options: [{ value: string }] }).options
+        ?.[0]?.value
 
-  if (error) {
-    return json({
-      type: InteractionResponseType.ChannelMessageWithSource,
-      data: {
-        content: "Server not initialized. Please run /init first.",
-        flags: 1 << 6,
+    const { error } = await CampaignService
+      .update(
+        discord_guild_id,
+        { notification_channel },
+      )
+
+    if (error) {
+      return {
+        application_id,
+
+        interaction_token,
+        body: {
+          content: "Server not initialized. Please run /init first.",
+        },
+      }
+    }
+
+    const content = "Notification Channel Updated!"
+    return {
+      application_id,
+      interaction_token,
+      body: {
+        content,
       },
-    })
-  }
-
-  const content = "Notification Channel Updated!"
-  return json({
-    type: InteractionResponseType.ChannelMessageWithSource,
-    data: {
-      content,
-      flags: 1 << 6,
-    },
-  })
+    }
+  }, true)
 }
 
 export default handleNotificationChannel
