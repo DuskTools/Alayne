7c7402d62bb4cb299548edcddba7d8bccd10c5a8
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-03-12 23:19:49 -0400

    Testing deferred response logic

--- supabase/functions/dusktools/_shared/deferredResponse.ts
+++ supabase/functions/dusktools/_shared/deferredResponse.ts
@@ -1,11 +1,16 @@
 import { InteractionResponseType } from "https://deno.land/x/discord_api_types@0.37.71/v10.ts"
 import { json } from "https://deno.land/x/sift@0.6.0/mod.ts"
 
-export default async (callback: () => unknown) => {
-  new Promise(callback).then((response) => {
+export default (callback: () => unknown) => {
+  const promise = new Promise((resolve) => {
+    resolve(callback)
+  })
+
+  promise.then((response) => {
     console.log("Deferred response sent")
     console.log(response)
   })
+
   return json({
     type: InteractionResponseType.DeferredChannelMessageWithSource,
   })
