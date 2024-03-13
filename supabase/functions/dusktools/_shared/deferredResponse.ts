23ded9c83ab731e929358f30ca453f706d6a2032
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-03-12 23:35:24 -0400

    More testing

--- supabase/functions/dusktools/_shared/deferredResponse.ts
+++ supabase/functions/dusktools/_shared/deferredResponse.ts
@@ -1,8 +1,8 @@
 import { InteractionResponseType } from "https://deno.land/x/discord_api_types@0.37.71/v10.ts"
 import { json } from "https://deno.land/x/sift@0.6.0/mod.ts"
 
-export default (callback: Promise<() => Record<string, unknown>>) => {
-  callback.then((response) => {
+export default (callback: () => Promise<Record<string, unknown>>) => {
+  Promise.resolve(callback()).then((response) => {
     console.log("Inside Deferred response")
     console.log(response)
   })
