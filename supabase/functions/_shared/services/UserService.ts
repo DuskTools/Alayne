ffe60bba35ed82eabd30d51135ef57887763d190
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-03-13 02:31:43 -0400

    WIP - adminClient

--- supabase/functions/_shared/services/UserService.ts
+++ supabase/functions/_shared/services/UserService.ts
@@ -1,8 +1,8 @@
-import { anonClient } from "../supabase/index.ts"
+import { adminClient } from "../supabase/index.ts"
 import { User } from "../supabase/types.ts"
 
 const create = async (createParams: User["Insert"]): Promise<User["Row"]> => {
-  const { data, error } = await anonClient
+  const { data, error } = await adminClient
     .from("users")
     .insert(createParams)
     .select()
@@ -18,7 +18,7 @@ const update = async (
   id: User["Row"]["id"],
   updateParams: User["Update"],
 ): Promise<User["Row"]> => {
-  const { data, error } = await anonClient
+  const { data, error } = await adminClient
     .from("users")
     .update(updateParams)
     .eq("id", id)
@@ -35,7 +35,7 @@ const update = async (
 const findByDiscordId = async ({
   discord_id,
 }: Pick<User["Row"], "discord_id">): Promise<User["Row"] | null> => {
-  const { data, error } = await anonClient
+  const { data, error } = await adminClient
     .from("users")
     .select()
     .eq("discord_id", discord_id)
